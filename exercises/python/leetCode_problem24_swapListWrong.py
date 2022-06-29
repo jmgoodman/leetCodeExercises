a5b23a8e99eb3e515381302c7fe909fc72e3e487
jmgoodman <j.mike.goodman@gmail.com>
2022-06-29 16:22:18 +0200

    Update leetCode_problem24_swapListWrong.py
    leveled up in pointer logic

--- exercises/python/leetCode_problem24_swapListWrong.py
+++ exercises/python/leetCode_problem24_swapListWrong.py
@@ -61,3 +61,46 @@ class Solution_nonCheating:
             head = first.next
             
         return res.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution_rehearsePointerLogic:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # okay, time to deploy my own solution to this
+        # think of "res" as a pointer to the value currently assigned to "head"
+        res = ListNode()
+        res.next = head
+        
+        # assign a variable "headPointer" which points to head (i.e., which gets res assigned to it)
+        headPointer = res
+        
+        # while head both has a value and points to a valid value
+        while head and head.next:
+            # alias the current two values: head and the value it points to, head.next
+            first  = head
+            second = head.next
+            
+            # now we want to rearrange how things point to each other
+            # first off, headPointer wants to point to second (since headPointer currently points to head, but if head and the value it points to are to be swapped...)
+            headPointer.next = second
+            
+            # second of all, we now want first to stop pointing to second but rather to circumvent it to get to the value second is POINTING to
+            first.next       = second.next
+            
+            # third of all, we now want second to point back to first (again, they *swap* positions, before first pointed to second, now...)
+            second.next      = first
+            
+            # now we wanna shift things around
+            # head should now point to the value that the element formerly known as "first" is NOW pointing to
+            head = first.next
+            
+            # and, in turn, headPointer should point to where head is pointing (therefore, it should be assigned to "first", which is currently head's pointer)
+            headPointer = first
+            
+            # and we contine along!
+            
+        return res.next # again, dereference res
